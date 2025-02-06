a949fe25bd4305adb8681afaeb717157e014aaee
djoreally <85000435+djoreally@users.noreply.github.com>
2025-02-06 17:04:14 +0000

    Update phone number in header component for accuracy

--- components/Header.tsx
+++ components/Header.tsx
@@ -61,7 +61,7 @@ const Header = () => {
           <div className="hidden md:flex items-center space-x-4">
             <a href="tel:1-800-555-MOMS" className="flex items-center text-primary-700 hover:text-primary-800">
               <Phone className="w-5 h-5 mr-1" />
-              <span>Call us at 1-800-555-MOMS</span>
+              <span>Call us at 1-215-767-2125</span>
             </a>
             <Link
               href="https://momsoilchange.bookyouroilchange.com/"
